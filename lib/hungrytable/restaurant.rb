7432e6a7176e1d00768753bc412d0b04894bb635
Nicholas Fine <nicholas.fine@gmail.com>
2012-05-16 16:29:58 -0500

    add #slotlock method to restaurant class

--- lib/hungrytable/restaurant.rb
+++ lib/hungrytable/restaurant.rb
@@ -12,5 +12,12 @@ module Hungrytable
       response = @access_token.get(uri)
       parse_json(response.body)
     end
+
+    def slotlock(options)
+      parse_uri_options(:restaurant_id, :date_time, :party_size, :time_security_id, :search_results_id, options)
+      uri = "/slotlock/?pid=#{ENV['OT_PARTNER_ID']}&st=0&"
+      response = @access_token.post(uri, {'RID' => @restaurant_id, 'datetime' => @date_time, 'partysize' => @party_size, 'timesecurityID' => @time_security_id, 'resultskey' => @search_results_id})
+      parse_json(response.body)
+    end
   end
 end
